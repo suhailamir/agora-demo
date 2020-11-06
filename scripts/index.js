b6248a8cb0441639a7d1d7c109bc2de598cc6a85
muhammad <muhammad.amir@xebia.com>
2020-11-06 23:10:40 +0400

    fix webpack

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const plugins = require('./plugins')
 
 const NODE_ENV = process.env.NODE_ENV
 
-const distPath = NODE_ENV == 'development' ? '../dist' : '../pages'
+const distPath = NODE_ENV == 'development' ? '../dist' : '../docs'
 
 module.exports = {
   entry: {
